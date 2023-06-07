a5d80020c9f417ad64561571283ec3a3b8750966
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2023-06-07 11:50:58 -0700

    Initial DB migration

--- api/models.py
+++ api/models.py
@@ -6,11 +6,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class ShortUrl (models.Model):
+    # Not explicitly needed, but it shuts up PyCharm CE
+    objects = models.Manager()
 
     # Django already provides an auth_user table, so just use it as foreign key
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    full_url = models.CharField(max_length=2048,
-                                db_comment="Full URL")
+    full_url = models.TextField(db_comment="Full URL")
     # MVP: randomly-generated code (4 chars) / Later: custom codes
     shortcode = models.CharField(primary_key=True, max_length=16,
                                  db_comment="Short code used for the shortened link. "
@@ -19,10 +20,13 @@ class ShortUrl (models.Model):
                                         db_comment="Date/time when the short URL was created.")
     is_deleted = models.BooleanField(default=False,
                                      db_comment="If True, the link is considered soft-deleted.")
-    expiry_time = models.DateTimeField(default=None,
+    expiry_time = models.DateTimeField(default=None, null=True,
                                        db_comment="When the short URL expires (optional).")
 
 
 class ShortUrlStats (models.Model):
+    # Not explicitly needed, but it shuts up PyCharm CE
+    objects = models.Manager()
+
     shortcode = models.OneToOneField(ShortUrl, primary_key=True, on_delete=models.CASCADE)
     times_accessed = models.IntegerField(db_comment="Number of times the URL was accessed.")
