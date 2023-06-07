976ff025f739a85ecd845fdc55898439cd62ea03
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2023-06-07 12:06:43 -0700

    Return HTTP 302 + full URL

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
+# TODO: this means a valid url is https://{domain}/api/{shortcode}. We want to get rid of the api/ bit
 urlpatterns = [
+    path('<str:shortcode>', views.get_url)
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import ShortUrl
 
 # Create your views here.
+# https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
+
+@api_view(['GET'])
+def get_url(request, shortcode: str):
+    """
+    Returns the full URL for the given short url.
+    """
+    try:
+        short_url = ShortUrl.objects.get(shortcode=shortcode)
+        return HttpResponseRedirect(short_url.full_url)
+    except:
+        return Http404("The requested link could not be found.")
